34ab278049b1e42c5c2a26f94bb82a461ba5a4d8
wewealka228 <wewepoweralka@mail.ru>
2024-10-17 00:09:53 +0300

    add new optioanal type

--- src/components/MainButton.tsx
+++ src/components/MainButton.tsx
@@ -2,6 +2,7 @@ type ButtonType = {
     name:string,
     callBack: () => void
     disabled?: boolean
+    className?:string
 }
 
 export function MainButton (props:ButtonType) {
@@ -13,6 +14,7 @@ const OnClickHandler = () => {
 
     return (
         <button
+        className={props.className}
         disabled ={props.disabled}
         onClick={OnClickHandler}
         >{props.name}</button>
