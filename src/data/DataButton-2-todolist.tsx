80f0210269ff491b453ab0d8be80b62e62a81d32
wewealka228 <wewepoweralka@mail.ru>
2024-10-22 13:13:27 +0300

    ref UFB && UAD && UCSB

--- src/data/DataButton-2-todolist.tsx
+++ src/data/DataButton-2-todolist.tsx
@@ -2,44 +2,27 @@ import { v1 } from "uuid";
 import { FiltersValuesType, TaskType } from "../layout/ConceptWindows"
 
 
-export const UsageRemoveBtn = (tasks: TaskType[], id: string): TaskType[] => {
+export const usageRemoveBtn = (tasks: TaskType[], id: string): TaskType[] => {
     return tasks.filter(task => task.id !== id);
 };
 
-export const UsageFilterBtn = (tasks: TaskType[], filter: FiltersValuesType) => {
-    if (filter === "Active") {
-        return tasks.filter(task => !task.isDone);
-    } else if (filter === "Completed") {
-        return tasks.filter(task => task.isDone);
-    } else {
-        return tasks;
-    }
+
+export const usageFilterBtn = (tasks: TaskType[], filter: FiltersValuesType) => {
+    return filter === "Active" ? tasks.filter(task => !task.isDone) :
+        filter === "Completed" ? tasks.filter(task => task.isDone) :
+            tasks;
 }
 
-    export const UsageAddTask = (tasks:TaskType[], title:string):TaskType[] => {
-        const NewTask: TaskType = {
-            id: v1(),
-            title: title,
-            isDone: false
-        };
-        return  [NewTask, ...tasks];
-    }
+export const usageAddTask = (tasks: TaskType[], title: string): TaskType[] => {
+    const newTask: TaskType = {
+        id: v1(),
+        title: title,
+        isDone: false
+    };
+    return [newTask, ...tasks];
+}
 
-    export const UsageChangeStatusBtn = (tasks: TaskType[], id: string, status:boolean):TaskType[] =>{
-        let stateForStatus = tasks.find(eachT=> eachT.id === id)
-        stateForStatus ? stateForStatus.isDone = status : console.log("bad news")
-        return [...tasks]
-    }
+export const usageChangeStatusBtn = (tasks: TaskType[], taskId: string) => {
+    return tasks.map(t => t.id === taskId ? { ...t, isDone: !t.isDone } : t)
+}
 
-
-
-// *****************************************************************************************************************************
-// export const UsageFilterBtn = (tasks: TaskType[], filter: FiltersValuesType) => {
-//     const filterConditions: { [key in FiltersValuesType]: (task: TaskType) => boolean } = {
-//         Active: task => !task.isDone,
-//         Completed: task => task.isDone,
-//         All: () => true,  // Возвращает все задачи
-//     };
-
-//     return tasks.filter(filterConditions[filter]);
-// };
