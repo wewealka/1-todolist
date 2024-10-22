02eff1b120af972d3eaae864cc7948e825c8c821
wewealka228 <wewepoweralka@mail.ru>
2024-10-22 13:20:37 +0300

    ref

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,9 +1,7 @@
 
 import styled from 'styled-components';
-import {  InitialTasks} from '../data/DataAppBLL-1-todolist';
+import { InitialTasks } from '../data/DataAppBLL-1-todolist';
 import { ConceptWindows } from '../layout/ConceptWindows';
-
-
 
 export const TodoList = () => {
     return (
@@ -21,24 +19,23 @@ export const TodoList = () => {
 
 const StyledTodoList = styled.div`
     display: flex;
-    flex-wrap: wrap; 
-    gap: 40px; 
-    justify-content: space-between; 
-    margin: 0; 
-    padding: 0; 
-
+    flex-wrap: wrap;
+    gap: 40px;
+    justify-content: space-between;
+    margin: 0;
+    padding: 0;
 `;
 
 const StyledCon = styled.div`
     margin: 30px;
-    padding: 10px; 
+    padding: 10px;
 `;
 
 const StyledTaskGroup = styled.div`
-    flex: 1 1 calc(25% - 20px); /* Задает ширину каждого блока (25% от родителя с учетом отступов) */
-    min-width: 200px; 
-    border: 1px solid black; 
-    border-radius: 5px; 
-    padding: 10px; 
+    flex: 1 1 calc(25% - 20px);
+    min-width: 200px;
+    border: 1px solid black;
+    border-radius: 5px;
+    padding: 10px;
     background-color: aqua;
 `;
