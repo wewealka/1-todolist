9e4a3aad0b792f0e8bdf79dde01be4cef9fdcbde
wewealka <wewepoweralka@mail.ru>
2025-01-13 19:49:57 +0300

    unitests &&  reducer for todolists

--- src/sate/todolist/todolist-reducer.test.ts
+++ src/sate/todolist/todolist-reducer.test.ts
@@ -1,87 +1,54 @@
+import {
+    AddTodolistAC,
+    ChangeFilterTodolistAC,
+    ChangeNameTodolistAC,
+    RemoveTodolistAC,
+    todolistReducer
+} from "./todolist-reducer";
+import {
+    FilterValuesType,
+    initialEXternalData,
+    initialINternalData,
+    todolistsType
+} from "../../mainVersion/associative-array/data/DataApp";
 
-import { todolistsReducer } from "./todolist-reducer";
-import { v1 } from "uuid";
-type FilterValuesType = "all" | "active" | "completed";
-type TaskType = {
-    id: string
-    title: string
-    isDone: boolean
-}
-type todolistsType = {
-    cwId: string
-    title: string
-    filter: FilterValuesType
-}
-const idCreater = () => {
-    return v1()
-}
 
-const keyListId_1 = idCreater()
-const keyListId_2 = idCreater()
-const keyListId_3 = idCreater()
-const keyListId_4 = idCreater()
-const keyListId_5 = idCreater()
-const keyListId_6 = idCreater()
+const startStateEX: Array<todolistsType> = (initialEXternalData);
+const startStateIN = (initialINternalData)
 
-const initialEXternalData: Array<todolistsType> = [
-    { cwId: keyListId_1, title: "What to learn", filter: 'all' },
-    { cwId: keyListId_2, title: "What to buy", filter: 'all' },
-    { cwId: keyListId_3, title: "What a Goal", filter: 'all' },
-    { cwId: keyListId_4, title: "device_by", filter: 'all' },
-    { cwId: keyListId_5, title: "AI make up", filter: 'all' },
-    { cwId: keyListId_6, title: "encrypt/decrypt", filter: 'all' },
-];
+test('correct todolist should be removed', () => {
+    const endState = todolistReducer(startStateEX, RemoveTodolistAC(initialEXternalData[0].cwId))
 
-const initialINternalData = {
-    [keyListId_1]: [
-        { id: v1(), title: "HTML", isDone: true },
-        { id: v1(), title: "CSS", isDone: true },
-        { id: v1(), title: "JS/TS", isDone: false },
-        { id: v1(), title: "Redux", isDone: false }
-    ],
-    [keyListId_2]: [
-        { id: v1(), title: "Cola", isDone: false },
-        { id: v1(), title: "Whiskey", isDone: false },
-        { id: v1(), title: "Ice", isDone: false },
-    ],
-    [keyListId_3]: [
-        { id: v1(), title: "Comp", isDone: false },
-        { id: v1(), title: "Mouse", isDone: false },
-        { id: v1(), title: "Keyboard", isDone: false },
-    ],
-    [keyListId_4]: [
-        { id: v1(), title: "Main", isDone: false },
-        { id: v1(), title: "Contacts", isDone: false },
-        { id: v1(), title: "Header", isDone: false },
-        { id: v1(), title: "About us", isDone: false },
-        { id: v1(), title: "Store", isDone: false },
-    ],
-    [keyListId_5]: [
-        { id: v1(), title: "Global", isDone: false },
-        { id: v1(), title: "Documents", isDone: false },
-        { id: v1(), title: "Start", isDone: false },
-        { id: v1(), title: "Mid game", isDone: false },
-        { id: v1(), title: "Late game", isDone: false },
-        { id: v1(), title: "WIN", isDone: false },
-    ],
-    [keyListId_6]: [
-        { id: v1(), title: "encrypt", isDone: true },
-        { id: v1(), title: "Documents", isDone: false },
-    ],
-}
-test('user reducer should incremet only age', () => {
+    expect(endState.length).toBe(5)
+    expect(endState[0].cwId).toBe(initialEXternalData[1].cwId)
+});
+test('correct todolist should be added', () => {
 
-    const startState: Array<todolistsType> = [
-        { cwId: keyListId_1, title: "What to learn", filter: 'all' },
-        { cwId: keyListId_2, title: "What to buy", filter: 'all' },
-        { cwId: keyListId_3, title: "What a Goal", filter: 'all' },
-        { cwId: keyListId_4, title: "device_by", filter: 'all' },
-        { cwId: keyListId_5, title: "AI make up", filter: 'all' },
-        { cwId: keyListId_6, title: "encrypt/decrypt", filter: 'all' },
-    ]
+    const newTodolistTtile = 'New Todolist Title'
 
-    const endState = todolistsReducer(startState, { type: 'REMOVE-TODOLIST', cwId: string })
+    const endState = todolistReducer(startStateEX, AddTodolistAC(newTodolistTtile))
 
-    expect(endState.length).toBe(4)
-    expect(endState[0].cwId).toBe()
-}); 
+    expect(endState.length).toBe(7)
+    expect(endState[0].title).toBe(newTodolistTtile)
+    expect(endState[0].filter).toBe('all')
+});
+test('correct todolist should change name', () => {
+
+    const newTodolistTitle = 'New Todolist Title'
+
+    const endState = todolistReducer(startStateEX, ChangeNameTodolistAC(initialEXternalData[0].cwId, newTodolistTitle))
+
+    expect(endState.length).toBe(6)
+    expect(endState[0].title).toBe(newTodolistTitle)
+    expect(endState[1].title).toBe('What to buy')
+});
+test('correct todolist filter should be changed', () => {
+
+    const newTodolistFilter: FilterValuesType = 'completed'
+
+    const endState = todolistReducer(startStateEX, ChangeFilterTodolistAC(initialEXternalData[0].cwId, newTodolistFilter))
+
+    expect(endState.length).toBe(6)
+    expect(endState[0].filter).toBe('completed')
+    expect(endState[1].title).toBe('What to buy')
+});
--- src/sate/todolist/todolist-reducer.ts
+++ src/sate/todolist/todolist-reducer.ts
@@ -1,20 +1,67 @@
-import { todolistsType } from "../../mainVersion/associative-array/data/DataApp"
+import {FilterValuesType, initialEXternalData, todolistsType} from "../../mainVersion/associative-array/data/DataApp"
+import {v1} from "uuid";
 
-type StateType = {
-    age: number
-    childrenCount: number
-    name: string
+export type RemoveTodolistActionType = {
+    type: 'REMOVE-TODOLIST'
+    cwId: string
+}
+export type AddTodolistActionType = {
+    type: 'ADD-TODOLIST'
+    title: string
+}
+export type ChangeTitleTodolistActionType = {
+    type: 'CHANGE-TODOLIST-TITLE'
+    cwId: string
+    title: string
+}
+export type ChangeFilterTodolistActionType = {
+    type: 'CHANGE-TODOLIST-FILTER'
+    cwId: string
+    filter: FilterValuesType
 }
 
-type ActionType = {
-    type: string
-    [key: string]: any
-}
+type ActionTypes =
+    RemoveTodolistActionType
+    | AddTodolistActionType
+    | ChangeTitleTodolistActionType
+    | ChangeFilterTodolistActionType
 
-export const todolistsReducer = (state: Array<todolistsType>, action: ActionType):Array<todolistsType> => {
+export const todolistReducer = (state: Array<todolistsType>, action: ActionTypes): Array<todolistsType> => {
     switch (action.type) {
-
+        case 'REMOVE-TODOLIST':
+            return state.filter(todolist => todolist.cwId !== action.cwId);
+        case 'ADD-TODOLIST':
+            return [{
+                cwId: v1(),
+                title: action.title,
+                filter: 'all',
+            }, ...state];
+        case 'CHANGE-TODOLIST-TITLE':
+            return state.map(t => t.cwId === action.cwId ? {...t, title: action.title} : t);
+        case'CHANGE-TODOLIST-FILTER':
+            return state.map(t => t.cwId === action.cwId ? {...t, filter: action.filter} : t);
         default:
             throw new Error("i don't understand this action type")
+    }
+}
+
+export const RemoveTodolistAC = (cwId: string): RemoveTodolistActionType => {
+    return {type: 'REMOVE-TODOLIST', cwId}
+}
+export const AddTodolistAC = (title: string): AddTodolistActionType => {
+    return {type: 'ADD-TODOLIST', title}
+}
+export const ChangeNameTodolistAC = (cwId: string, title: string): ChangeTitleTodolistActionType => {
+    return {
+        type: 'CHANGE-TODOLIST-TITLE',
+        cwId,
+        title
+    }
+}
+export const ChangeFilterTodolistAC = (cwId: string, filter: FilterValuesType): ChangeFilterTodolistActionType => {
+    return {
+        type: 'CHANGE-TODOLIST-FILTER',
+        cwId,
+        filter
     }
 }
